d7fd7f2ea432197cf29860e025ec44364ff680ca
mariomarroquin3 <mario.marroquin.2007@gmail.com>
2026-01-02 17:23:08 -0600

    edit

--- vite.config.ts
+++ vite.config.ts
@@ -5,18 +5,11 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/a-digital-bloom/", // 🔴 CLAVE PARA GITHUB PAGES
-
   server: {
     host: "::",
     port: 8080,
   },
-
-  plugins: [
-    react(),
-    mode === "development" && componentTagger(),
-  ].filter(Boolean),
-
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
